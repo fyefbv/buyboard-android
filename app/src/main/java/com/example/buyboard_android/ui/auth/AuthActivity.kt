d8bf4c8231a9ce8c1786cb5044a0fc6c7fff0b93
Roman <roman.gavr10@gmail.com>
2025-11-10 00:16:45 +0300

    feat(auth): сделано отображение фрагментов регистрации и аутентификации на AuthActivity и добавлен переход между ними

--- app/src/main/java/com/example/buyboard_android/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/example/buyboard_android/ui/auth/AuthActivity.kt
@@ -1,15 +1,44 @@
 package com.example.buyboard_android.ui.auth
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.buyboard_android.R
+import com.example.buyboard_android.databinding.ActivityAuthBinding
+import com.example.buyboard_android.ui.auth.login.LoginFragment
+import com.example.buyboard_android.ui.auth.register.RegisterFragment
 
-class AuthActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity(),
+    LoginFragment.LoginListener,
+    RegisterFragment.RegisterListener {
+    private lateinit var activityAuthBinding: ActivityAuthBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_auth)
+        activityAuthBinding = ActivityAuthBinding.inflate(layoutInflater)
+        setContentView(activityAuthBinding.root)
+
+        showLoginFragment()
+    }
+
+    private fun showLoginFragment() {
+        val fragment = LoginFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(activityAuthBinding.authFragmentContainer.id, fragment)
+            .commit()
+    }
+
+    private fun showRegisterFragment() {
+        val fragment = RegisterFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(activityAuthBinding.authFragmentContainer.id, fragment)
+            .addToBackStack("register")
+            .commit()
+    }
+
+    override fun onRegisterClicked() {
+        showRegisterFragment()
+    }
+
+    override fun onLoginClicked() {
+        supportFragmentManager.popBackStack()
     }
 }
